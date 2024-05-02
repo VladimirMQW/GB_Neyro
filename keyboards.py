bf6955771e6b7e46938d72b4c6b5ce840f20eaa5
vmqw <vmqwf@yandex.ru>
2024-05-03 00:24:17 +0300

    Добавка кнопок и фильтра

---
+++ keyboards.py
@@ -0,0 +1,13 @@
+from aiogram import types
+
+button1 = types.KeyboardButton(text="/start")
+button2 = types.KeyboardButton(text="/stop")
+button3 = types.KeyboardButton(text="/info")
+button4 = types.KeyboardButton(text="/user")
+button5 = types.KeyboardButton(text="/help")
+
+
+keyboard1 = [[button3, button4, button5], [button1, button2]]
+
+
+keyboard = types.ReplyKeyboardMarkup(keyboard=keyboard1, resize_keyboard=True)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 import config
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
 import logging
 import random
+from keyboards import keyboard
+
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
@@ -16,21 +18,23 @@ dp = Dispatcher()
 @dp.message(Command(commands=["start"]))
 async def start(message: types.Message):
     await message.answer(
-        f"Привет, {message.from_user.full_name}! Это бот для чата. Введи /help для справки"
+        f"Привет, {message.from_user.full_name}! Это бот для чата. Введи /help для справки",
+        reply_markup=keyboard,
     )
 
 
-@dp.message(Command(commands=["stop"]))
-async def start(message: types.Message):
+@dp.message(Command(commands=["stop", "стоп"]))
+async def stop(message: types.Message):
     print(message.from_user.first_name)
     await message.answer(f"Пока, {message.chat.first_name}!")
     await bot.close()
 
 
-@dp.message(Command(commands=["info"]))
+@dp.message(Command(commands=["info", "инфо"]))
 async def send_info(message: types.Message):
     await message.answer(
-        "Это простой телеграм бот и может ответить на несколько команд. Подробнее /help."
+        "Это простой телеграм бот и может ответить на несколько команд. Подробнее /help.",
+        reply_markup=keyboard,
     )
 
 
@@ -46,6 +50,22 @@ async def send_user_info(message: types.Message):
 async def send_user_info(message: types.Message):
     user = message.from_user
     await message.answer(f"Бот понимает команды: /start, /stop, /info, /help, /user.")
+
+
+@dp.message(F.text)
+async def msg(message: types.Message):
+    if (
+        "привет" in message.text.lower()
+    ):  # ["привет", "здаров", "здравствуй", "здравствуйте"]
+        await message.reply("И тебе привет!")
+    elif "как дела" in message.text.lower():
+        await message.reply("Норм, а у тебя?")
+    elif "хорошо" in message.text.lower():
+        await message.reply("Ну и отлично!")
+    elif "пока" in message.text.lower():
+        await message.reply("И вам не хворать!")
+    else:
+        await message.reply("Не понимаю тебя...")
 
 
 async def main():
